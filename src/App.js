beb737561bef2b143978d11aa680b0e5fa6b25e4
Doc Savage <97268006+DocSavage11@users.noreply.github.com>
2022-07-18 19:39:02 -0500

    Nav, first header section done. Responsivness next

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 
 import Nav from './Components/Main Components/Nav Bar/Nav';
+import Landing from './Pages/Main Pages/Landing/Landing';
 
 function App() {
   return (
     <>
       <Nav />
+
+      <Landing />
     </>
   );
 }
---
+++ src/Pages/Main Pages/Landing/Landing.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Landing.css';
+
+
+
+const Landing = () => {
+  return (
+    <div className="Landing-Section">
+        <img src={require('../../../Assets/Images/Landing Page Images/Introduction-Section-Image.png')} alt="" id="Intro-Image" />
+
+        <div className="Introduction-Information-Box">
+            <h1 id='Intro-Header-Title'>Introducing Snapchat+</h1>
+
+            <p id="Intro-Header-SubTitle">Be first to access unique and pre-release features for only $3.99/month.</p>
+
+            <a href="/">
+                <button className='Learn-More-Button'>
+                    <p>Learn More</p>
+                </button>
+            </a>
+        </div>
+    </div>
+  )
+}
+
+export default Landing
