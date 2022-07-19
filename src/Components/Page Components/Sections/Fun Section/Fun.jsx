eed306ac6966c642da8a19e07f3ab5407df00128
Doc Savage <97268006+DocSavage11@users.noreply.github.com>
2022-07-19 16:06:21 -0500

    Sus

---
+++ src/Components/Page Components/Sections/Fun Section/Fun.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import './Fun.css'
+
+const Fun = () => {
+  return (
+    <div className="Fun-Section">
+        <div className="Video-Wrapper">
+            <video src={require('../../../../Assets/Videos/Landing Page Videos/Have-Fun-Video.mp4')} alt="" id="Fun-Video" autoPlay loop></video>
+        </div>
+        
+        <div data-aos="fade-up" data-aos-duration="1000" className="Phone-Infromation-Section">
+            <h1 id="Section-Title">
+            Have fun with your friends and family.
+            </h1>
+
+            <form className='Phone-Inputs' action="">
+
+                <fieldset>
+                    <select name="AreaCodes" id="Area-Code-Selection">
+                        <option id='AreaCodeItem' value="AU"><p>AU +61</p></option>
+                        <option id='AreaCodeItem' value="BR"><p>BR +55</p></option>
+                        <option id='AreaCodeItem' value="GB"><p>GB +44</p></option>
+                        <option id='AreaCodeItem' value="IN"><p>IN +91</p></option>
+                        <option id='AreaCodeItem' value="MX"><p>MX +52</p></option>
+                        <option id='AreaCodeItem' value="US"><p>US +1</p></option>
+                    </select>
+
+                    <input id='Phone-Number-Input-Box' type="text" name='Phone' placeholder='Phone Number' />
+
+
+                
+                    <a href="/">
+                        <button className='Try-Now-Button'>
+                            <p>Find Your Friends</p>
+                        </button>
+                    </a>
+             </fieldset>
+            </form>
+        </div>
+
+
+        <div className="Fun-Video-Large-Wrapper">
+            <div data-aos="fade-up" data-aos-duration="1000" className="Fun-Video-Overlay-Card">
+                <div className="Discover">
+                    <img src={require('../../../../Assets/Icons/Expand-Arrow-Up.png')} alt="" id="Up-Arrow" className='Expand-Arrow-Fun-Video'/>
+                    <p id="DiscoverText">Discover Snapchat</p>
+                </div>
+                
+                <h1 id="Video-Title">
+                    Express yourself with millions of Lenses.
+                </h1>
+
+                <a href="/">
+                    <button className='Try-Now-Button-Video'>
+                        <p>Find Your Friends</p>
+                    </button>
+                </a>
+            </div>
+            <video src={require('../../../../Assets/Videos/Landing Page Videos/Have-Fun-Video.mp4')} alt="" id="Fun-Video-Large" autoPlay loop>
+            </video>
+        </div>
+    </div>
+  )
+}
+
+export default Fun
--- src/Pages/Main Pages/Landing/Landing.jsx
+++ src/Pages/Main Pages/Landing/Landing.jsx
@@ -5,6 +5,7 @@ import './Landing.css';
 import Nav from '../../../Components/Main Components/Nav Bar/Nav';
 import Introduction from '../../../Components/Page Components/Sections/Indroduction Section/Introduction';
 import Express from '../../../Components/Page Components/Sections/Express Section/Express';
+import Fun from '../../../Components/Page Components/Sections/Fun Section/Fun';
 
 
 
@@ -14,6 +15,7 @@ const Landing = () => {
         <Nav />
         <Introduction />
         <Express />
+        <Fun />
     </>
   )
 }
