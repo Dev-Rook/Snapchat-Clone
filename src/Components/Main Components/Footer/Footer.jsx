4ae89b7ad23bdc317a10db5563bc8754d1784e0f
Doc Savage <97268006+DocSavage11@users.noreply.github.com>
2022-07-22 13:22:32 -0500

    Stuff

--- src/Components/Main Components/Footer/Footer.jsx
+++ src/Components/Main Components/Footer/Footer.jsx
@@ -5,33 +5,50 @@ const Footer = () => {
   return (
     <footer className='Footer'>
         <div className="CCA">
-            <ul id="Company-Links">
+            <ul className="Label-Links First-Label">
                 <span>Company</span>
-                <li><p>Snap Inc.</p></li>
-                <li><p>Snap Inc.</p></li>
-                <li><p>Snap Inc.</p></li>
-                <li><p></p></li>
+                <li><a href='#'>Snap Inc.</a></li>
+                <li><a href='#'>Careers</a></li>
+                <li><a href='#'>News</a></li>
+                <li><a href='#'></a></li>
             </ul>
 
-            <ul id="Community-Links">
+            <ul className="Label-Links">
                 <span>Community</span>
-                <li><p>Snap Inc.</p></li>
-                <li><p>Snap Inc.</p></li>
-                <li><p>Snap Inc.</p></li>
-                <li><p>Snap Inc.</p></li>
+                <li><a href='#'>Snapchat Support</a></li>
+                <li><a href='#'>Pixy Support</a></li>
+                <li><a href='#'>Community Guidelines</a></li>
+                <li><a href='#'>Safety Center</a></li>
             </ul>
 
-            <ul id="Advertising-Links">
+            <ul className="Label-Links">
                 <span>Advertising</span>
-                <li><p>Snap Inc.</p></li>
-                <li><p>Snap Inc.</p></li>
-                <li><p>Snap Inc.</p></li>
-                <li><p>Snap Inc.</p></li>
+                <li><a href='#'>Buy Ads</a></li>
+                <li><a href='#'>Advertising Policies</a></li>
+                <li><a href='#'>Political Ads Library</a></li>
+                <li><a href='#'>Brand Guidelines</a></li>
+                <li><a href='#'>Promotions Rules</a></li>
             </ul>
         </div>
 
         <div className="LL">
+            <ul className="Label-Links">
+                <span>Legal</span>
+                <li><a href="#">Privacy Center</a></li>
+                <li><a href="#">Cookie Policy</a></li>
+                <li><a href="#">Cookie Settings</a></li>
+                <li><a href="#">Report Infringement</a></li>
+                <li><a href="#">Custom Creative Tools Terms</a></li>
+                <li><a href="#">Community Geofilter Terms</a></li>
+                <li><a href="#">Lens Studio Terms</a></li>
+            </ul>
 
+            <div className="Language-Selector">
+                <span>Language</span>
+
+                <select name="Languages" id="Box
+                "></select>
+            </div>
         </div>
     </footer>
   )
